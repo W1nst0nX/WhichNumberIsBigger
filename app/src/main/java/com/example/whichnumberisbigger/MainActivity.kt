12ea2d2382662eff058d3ee4c0e276229e4fb829
w1nst0nxu <winstonx2004@gmail.com>
2020-08-24 11:10:02 -0700

    Finished Project

--- app/src/main/java/com/example/whichnumberisbigger/MainActivity.kt
+++ app/src/main/java/com/example/whichnumberisbigger/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.whichnumberisbigger
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+private var firstNumber = 0
+private var secondNumber = 0
+private var score = 0
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        start()
+    }
+
+    fun onClickLeft(view: View) {
+        if(firstNumber > secondNumber)
+        {
+            score++
+            Toast.makeText(this, "Congrats, it's right!", Toast.LENGTH_SHORT).show()
+            start()
+        }
+        else
+        {
+            score--
+            Toast.makeText(this, "Wow, how can you be so bad?", Toast.LENGTH_SHORT).show()
+            start()
+        }
+    }
+
+    fun onClickRight(view: View) {
+        if(firstNumber < secondNumber)
+        {
+            score++
+            Toast.makeText(this, "Congrats, it's right!", Toast.LENGTH_SHORT).show()
+            start()
+        }
+        else
+        {
+            score--
+            Toast.makeText(this, "Wow, how can you be so bad?", Toast.LENGTH_SHORT).show()
+            start()
+        }
+    }
+
+    fun start() {
+        firstNumber = (Math.random() * 100).toInt()
+        secondNumber = (Math.random() * 100).toInt()
+        while(firstNumber == secondNumber)
+        {
+            secondNumber = (Math.random() * 100).toInt()
+        }
+        textView_main_score.text = score.toString()
+        button_main_left.text = firstNumber.toString()
+        button_main_right.text = secondNumber.toString()
     }
 }
